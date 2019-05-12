48b352bc6a1339ca2ba64c26f88b276f4cecf360
jbryl7 <37078470+jbryl7@users.noreply.github.com>
2019-05-12 19:45:42 +0200

    visual change

--- src/main/scala/battleships/Cell.scala
+++ src/main/scala/battleships/Cell.scala
@@ -1,6 +1,10 @@
 package main.scala.battleships
 
-case class Cell(x: Int, y: Int, state: State.Value = State.Water){
+case class Cell(x: Int, y: Int, state: State.Value)
+
+object Cell {
+  def apply(x: Int, y: Int): Cell = 
+    Cell(x, y, State.Water)
 }
 object State extends Enumeration {
   val Water, Hit, Miss, Occupied, Sink = Value
